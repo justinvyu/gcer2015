27a6c5423674b8ba10ba1c3d34499ae3bb7e4d61
AlexLin1234 <skyloft0629@gmail.com>
2015-06-14 19:52:25 -0700

    Changed Main.c
    added thread stuff

--- Link/Main.c
+++ Link/Main.c
@@ -5,11 +5,33 @@
 #define left_motor 0
 #define right_motor 3
 #define claw_servo 13
+#define half_claw 1200
+#define full_claw 2400
+	void half() { 
+
+	servo_set(claw_servo, half_claw, 1000);
+	
+	}
+	void full(){
+	servo_set(claw_servo, full_claw, 1000);
+		
+		}
+
 void ping_get(){
+	thread half_tid = thread_create(half);
+	thread full_tid = thread_create(full);
 	motor(left_motor, -40);
 	motor(right_motor, -40);
-	servo_set(claw_servo, 1200, 1000);
+	thread_start(half_tid);
 	msleep(1000);
+	thread_wait(half_tid);
+	thread_destroy(half_tid);
+	motor(left_motor, 40);
+	motor(right_motor, 40);
+	thread_start(full_tid);
+	msleep(1000);
+	thread_wait(full_tid);
+	thread_destroy(full_tid);
 	
 	
 	
