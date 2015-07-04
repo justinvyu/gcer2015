22f16b278b3d5b62c21f7d7cb6a5d5aaab979283
tommyy96 <tommy2yang@gmail.com>
2015-07-04 11:11:50 -0700

    updated code

--- linkDE/functions.h
+++ linkDE/functions.h
@@ -7,15 +7,15 @@
 #define PICK_SERVO 0
 #define PICK_START 780
 #define PICK_UP 0
-#define PICK_DOWN 1340
+#define PICK_DOWN 1420
 #define PICK_PUSH 2047
 #define SORT_SERVO 1
-#define RED 400
+#define RED 200
 #define START 880
-#define GREEN 1400
+#define GREEN 1600
 #define GREEN_SERVO 2
 #define GREEN_START 100
-#define GREEN_DUMP 720
+#define GREEN_DUMP 900
 #define RED_SERVO 3
 #define RED_START 1400
 #define RED_DUMP 700
@@ -55,19 +55,20 @@ void sort(){
 	float curr_time = seconds();
 	while(curr_time - start_time < 3){
 		curr_time = seconds();
-		slow_servo(SORT_SERVO,START,1);
 		printf("In loop\n");
 		camera_update();
 		if(get_object_area(0,1) != -1){
 			slow_servo(SORT_SERVO,GREEN,0.5);
 			printf("GREEN\n");
 			msleep(2000);
+			slow_servo(SORT_SERVO,START,1);
 			break;
 		}
 		else if(get_object_area(1,1) != -1){
 			slow_servo(SORT_SERVO,RED,0.5);
 			printf("RED\n");
 			msleep(2000);
+			slow_servo(SORT_SERVO,START,1);
 			break;
 		}
 		else{
@@ -90,7 +91,12 @@ void full_sort(){
 	while(i < 6){
 		forward(15);
 		right(3,0);
-		backward(1.2);
+		if(i < 4){
+			backward(0.75);
+		}
+		else{
+			forward(2);
+		}
 		msleep(500);
 		left(3,0);
 		ssp(PICK_SERVO,PICK_DOWN);
@@ -100,20 +106,21 @@ void full_sort(){
 		ssp(PICK_SERVO,PICK_UP);
 		msleep(250);
 		sort();
-		backward(16);
+		right(4,0);
+		backward(15);
 		i++;
 	}
 	reset();
 }
 
 void other_side(){
-	backward(20);
+	backward(8);
 	ssp(RED_SERVO,RED_DUMP);
-	msleep(1000);
+	msleep(1500);
 	ssp(RED_SERVO,RED_START);
 	forward(90);
 	right(120,0);
-	backward(25);
+	backward(32);
 }
 
 #endif
--- linkDE/main.c
+++ linkDE/main.c
@@ -6,12 +6,12 @@
 
 #define PICK_SERVO 0
 #define SORT_SERVO 1
-#define RED 400
+#define RED 200
 #define START 880
-#define GREEN 1400
+#define GREEN 1600
 #define GREEN_SERVO 2
 #define GREEN_START 100
-#define GREEN_DUMP 720
+#define GREEN_DUMP 900
 #define RED_SERVO 3
 #define RED_START 1400
 #define RED_DUMP 700
@@ -24,25 +24,25 @@ int main()
 	ssp(RED_SERVO,RED_START);
 	ssp(GREEN_SERVO,GREEN_START);
 	
-	//wait_for_light(0);
+	wait_for_light(0);
+	start();
 	shut_down_in(119);
 	
 	enable_servos();
-	reset();
-	
 	msleep(5000);
+	reset();
 	
 	right(47,0);
 	forward(35);
 	right(10,0);
-	backward(42);
+	backward(46);
 	full_sort();
-	right(10,0);
 	other_side();
 	ssp(GREEN_SERVO,GREEN_DUMP);
 	msleep(1000);
 	ssp(GREEN_SERVO,GREEN_START);
 	msleep(250);
+	now();
 	ao();
 	disable_servos();
 }
